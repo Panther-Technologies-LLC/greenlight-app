86cd3fe6642a747502a9119871d4e68cb2ab1fe2
Jennice <jennicelodi@gmail.com>
2022-04-27 13:55:32 -0700

    imported font awesome, combined header and nav, added footer, added css file

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,12 +1,13 @@
 import React from "react"
+import './App.css'
 import Home from './pages/Home'
 import Header from './components/Header'
 import Footer from './components/Footer'
-import Navigation from './components/Navigation'
 import PitchCardIndex from './pages/PitchCardIndex'
 import PitchCardShow from './pages/PitchCardShow'
 import PitchCardNew from './pages/PitchCardNew'
 import PitchCardEdit from './pages/PitchCardEdit'
+
 import {
   BrowserRouter as  Router,
   Route,
@@ -82,8 +83,7 @@ class App extends React.Component {
     return(
       <>
         <Router>
-          <Header />
-          <Navigation {...this.props}/>
+          <Header {...this.props} />
             <Switch>
               <Route exact path="/" component={Home} />
               {logged_in &&
--- app/javascript/components/components/Header.js
+++ app/javascript/components/components/Header.js
@@ -4,11 +4,33 @@ import { NavLink } from 'react-router-dom'
 
 class Header extends Component {
   render() {
-
+    const{
+      logged_in,
+      current_user,
+      new_user_route,
+      sign_in_route,
+      sign_out_route
+    } = this.props
     return(
-      <>
-        <h3>Greenlight App</h3>
-      </>
+
+      <Nav>
+        <NavItem>
+          <NavLink to="/" className="nav-link">Home</NavLink>
+        </NavItem>
+        {logged_in &&
+          <NavItem>
+            <a href={sign_out_route} className="nav-link">Logout</a>
+            <NavLink to="/pitchcards" className="nav-link">Pitch Cards</NavLink>
+            <NavLink to="/pitchcardnew" className="nav-link">Create Pitch Card</NavLink>
+          </NavItem>
+        }
+        {!logged_in &&
+          <NavItem>
+            <a href={new_user_route} className="nav-link">Create Account</a>
+            <a href={sign_in_route} className="nav-link">Login</a>
+          </NavItem>
+        }
+      </Nav>
     )
   }
 }
