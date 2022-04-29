ddc436d8027d5a220e3d5d11d1ec6c9820b054ff
Jennice <jennicelodi@gmail.com>
2022-04-29 02:28:39 -0700

    navbar styling in progress

--- app/javascript/components/components/Header.js
+++ app/javascript/components/components/Header.js
@@ -10,91 +10,104 @@ import {
   DropdownItem,
   NavbarText,
   Nav,
-  NavItem }
+  NavItem,
+  Button
+}
   from 'reactstrap'
 import { NavLink } from 'react-router-dom'
 
 class Header extends Component {
   render() {
-    const{
+    const {
       logged_in,
       current_user,
       new_user_route,
       sign_in_route,
       sign_out_route
     } = this.props
-    return(
+    return (
       <>
-      <div>
-        <Navbar
-          color="light"
-          expand="md"
-          fixed="top"
-          light
-        >
-          <NavbarBrand id="nav" href="/" >
-            Greenlight
-          </NavbarBrand>
-          <NavbarToggler onClick={function noRefCheck(){}} />
-          <Collapse navbar>
+        <div>
+          <Navbar
+            color="light"
+            expand="md"
+            fixed="top"
+            light
+          >
+            <NavbarBrand id="nav" href="/" >
+              <i className="fa-solid fa-lightbulb green"></i> Greenlight
+            </NavbarBrand>
+            <NavbarToggler onClick={function noRefCheck() { }} />
+            <Collapse navbar>
+              <NavbarText>
+                {logged_in &&
+                  <div>
+                    Signed in as {current_user.email}
+                  </div>}
+              </NavbarText>
+
+            </Collapse>
             <Nav
-              className="me-auto"
+              className="me-auto vert-center"
               navbar
             >
-              {logged_in &&
-              <UncontrolledDropdown
-                inNavbar
-                nav
-              >
-                <DropdownToggle
-                  caret
-                  nav
-                >
-                  Profile
-                </DropdownToggle>
-                <DropdownMenu end>
-                  <DropdownItem>
-                    <NavLink to="/userprofilenew" className="nav-link">Create Your Profile</NavLink>
-                  </DropdownItem>
-                  <DropdownItem>
-                    <NavLink to="/pitchcardnew" className="nav-link">Create Pitch Card</NavLink>
-                  </DropdownItem>
-                  <DropdownItem>
-                    <a href="/greenlightindex" className="nav-link">Greenlight List</a>
-                  </DropdownItem>
-                  <DropdownItem>
-                    <a href={sign_out_route} className="nav-link">Logout</a>
-                  </DropdownItem>
-                  <DropdownItem divider />
-                </DropdownMenu>
-              </UncontrolledDropdown>
-              }
               {logged_in &&
                 <NavItem>
                   <NavLink to="/pitchcards" className="nav-link">Pitch Cards</NavLink>
                 </NavItem>
               }
-              {!logged_in &&
-                <NavItem>
-                  <a href={new_user_route} className="nav-link">Create Account</a>
-                </NavItem>
+              {logged_in &&
+                <UncontrolledDropdown
+                  inNavbar
+                  nav
+                >
+                  <DropdownToggle
+                    caret
+                    nav
+                  >
+                    <i className="fa-solid fa-user green nav-icon"></i>
+                  </DropdownToggle>
+                  <DropdownMenu
+                    end
+                    className="dropdown-right"
+                  >
+                    <DropdownItem>
+                      <NavLink to="/userprofilenew" className="nav-link">Create Your Profile</NavLink>
+                    </DropdownItem>
+                    <DropdownItem>
+                      <NavLink to="/pitchcardnew" className="nav-link">Create Pitch Card</NavLink>
+                    </DropdownItem>
+                    <DropdownItem>
+                      <a href="/greenlightindex" className="nav-link">Greenlight List</a>
+                    </DropdownItem>
+                  </DropdownMenu>
+                </UncontrolledDropdown>
               }
               {!logged_in &&
-                <NavItem>
-                  <a href={sign_in_route} className="nav-link">Login</a>
-                </NavItem>
+                <NavLink to={new_user_route} className="nav-link">
+                  <Button className="signin-btn">
+                    Sign-up
+                  </Button>
+                </NavLink>
+              }
+              {!logged_in &&
+                <NavLink to={sign_in_route} className="nav-link">
+                  <Button className="signin-btn">
+                    Sign-in
+                  </Button>
+                </NavLink>
+              }
+              {logged_in &&
+                <NavLink to={sign_out_route} className="nav-link">
+                  <Button className="signout-btn">
+                    Sign-out
+                  </Button>
+                </NavLink>
               }
             </Nav>
-            <NavbarText>
-              {logged_in && 
-                <div>
-                  Logged in as {current_user.email}
-                </div> }
-            </NavbarText>
-          </Collapse>
-        </Navbar>
-      </div>
-    </>
+          </Navbar>
+        </div>
+      </>
     )
   }
 }
