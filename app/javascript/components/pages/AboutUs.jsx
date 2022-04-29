5a9334643d760bdf7836fb5396933623bde4462a
Nathaniel Bethea <jumpnate23@outlook.com>
2022-04-29 13:38:56 -0700

    added about us page, fixed some styling

---
+++ app/javascript/components/pages/AboutUs.jsx
@@ -0,0 +1,64 @@
+import React, { Component } from "react";
+
+
+// function Contact()
+class AboutUs extends Component {
+  render() {
+      return (
+        <div class=&#34;about-section&#34;>
+  <h1>About Us Page</h1>
+  <p>Some text about who we are and what we do.</p>
+  <p>Resize the browser window to see that this page is responsive by the way.</p>
+</div>
+
+<h2 style=&#34;text-align:center&#34;>Our Team</h2>
+<div class=&#34;row&#34;>
+  <div class=&#34;column&#34;>
+    <div class=&#34;card&#34;>
+      <img src=&#34;/w3images/team1.jpg&#34; alt=&#34;Jane&#34; style=&#34;width:100%&#34;>
+      <div class=&#34;container&#34;>
+        <h2>Jane Doe</h2>
+        <p class=&#34;title&#34;>CEO & Founder</p>
+        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+        <p>jane@example.com</p>
+        <p><button class=&#34;button&#34;>Contact</button></p>
+      </div>
+    </div>
+  </div>
+
+  <div class=&#34;column&#34;>
+    <div class=&#34;card&#34;>
+      <img src=&#34;/w3images/team2.jpg&#34; alt=&#34;Mike&#34; style=&#34;width:100%&#34;>
+      <div class=&#34;container&#34;>
+        <h2>Mike Ross</h2>
+        <p class=&#34;title&#34;>Art Director</p>
+        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+        <p>mike@example.com</p>
+        <p><button class=&#34;button&#34;>Contact</button></p>
+      </div>
+    </div>
+  </div>
+  
+  <div class=&#34;column&#34;>
+    <div class=&#34;card&#34;>
+      <img src=&#34;/w3images/team3.jpg&#34; alt=&#34;John&#34; style=&#34;width:100%&#34;>
+      <div class=&#34;container&#34;>
+        <h2>John Doe</h2>
+        <p class=&#34;title&#34;>Designer</p>
+        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+        <p>john@example.com</p>
+        <p><button class=&#34;button&#34;>Contact</button></p>
+      </div>
+    </div>
+  </div>
+</div>
+
+</body>
+</html>"
+
+document.write(info)
+</script >
+  };
+}
+
+export default AboutUs;
